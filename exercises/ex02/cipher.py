d5b1756a7f7c55934fcd9fab199f7d2ccffc7da3
Nathaniel Choiniere <choiniere@unc.edu>
2020-09-01 17:52:03 -0400

    Finished cipher.py

--- exercises/ex02/cipher.py
+++ exercises/ex02/cipher.py
@@ -2,16 +2,18 @@
 
 __author__ = "730443739"
 
-def encode_char(char: str):
+
+def encode_char(char: str) -> str:
     """This function encodes a single-lettered string by a shift value of 1."""
     charLower: str = char.lower()
     ascii: int = ord(charLower)
     normalized_ascii: int = ascii - 97
     encoded_ascii: int = (normalized_ascii + 1) % 26 + 97
     encoded_char: str = chr(encoded_ascii)
-    print(encoded_char)
+    return encoded_char
 
-def encode_str(string: str):
+
+def encode_str(string: str) -> str:
     """This function encodes a 4-letter string by a shift value of 1."""
     strLower: str = string.lower()
     ascii0: int = ord(strLower[0])
@@ -27,18 +29,20 @@ def encode_str(string: str):
     encoded_ascii2: int = (normalized_ascii2 + 1) % 26 + 97
     encoded_ascii3: int = (normalized_ascii3 + 1) % 26 + 97
     encoded_str: str = (chr(encoded_ascii0) + chr(encoded_ascii1) + chr(encoded_ascii2) + chr(encoded_ascii3))
-    print(encoded_str)
+    return encoded_str
 
-def decode_char(char: str):
+
+def decode_char(char: str) -> str:
     """This function decodes a single-lettered string by a shift value of 1."""
     charLower: str = char.lower()
     ascii: int = ord(charLower)
     normalized_ascii: int = ascii - 97
     decoded_ascii: int = (normalized_ascii - 1) % 26 + 97
     decoded_char: str = chr(decoded_ascii)
-    print(decoded_char)
+    return decoded_char
 
-def decode_str(string: str):
+
+def decode_str(string: str) -> str:
     """This function encodes a 4-letter string by a shift value of 1."""
     strLower: str = string.lower()
     ascii0: int = ord(strLower[0])
@@ -54,4 +58,4 @@ def decode_str(string: str):
     decoded_ascii2: int = (normalized_ascii2 - 1) % 26 + 97
     decoded_ascii3: int = (normalized_ascii3 - 1) % 26 + 97
     decoded_str: str = (chr(decoded_ascii0) + chr(decoded_ascii1) + chr(decoded_ascii2) + chr(decoded_ascii3))
-    print(decoded_str)
+    return decoded_str

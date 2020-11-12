51392c76415f7ab76fbcb9e02d184b70bc09cdd1
Nate Choiniere <choinierenate@gmail.com>
2020-11-12 12:47:56 -0500

    Pj02

---
+++ lessons/ls44_art.py
@@ -0,0 +1,34 @@
+import turtle as t
+from random import random
+
+LEFT = 1.0
+RIGHT = -1.0
+TRUNK = 100.0
+UP = 90.0
+
+random_float: float = random()
+
+def tree(x: float, y: float) -> None:
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+    branch(TRUNK, 90.0)
+    t.update()
+
+
+def branch(length: float, angle: float) -> None:
+    t.setheading(angle)
+    t.forward(length)
+    if length < 3:
+        ...
+    else:
+        branch(length * random_float, angle + 25)
+        branch(length * random_float, angle - 25)
+    t.setheading(angle + 180)
+    t.forward(length)
+
+if __name__ == "__main__":
+    t.speed(0)
+    t.tracer(0, 0)
+    t.getscreen().onclick(tree)
+    t.done()
